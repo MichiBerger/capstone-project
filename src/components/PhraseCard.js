24fb51b0df45a6b27c769a8849fb4917fe49b4b8
Michi Berger <mberger83@yahoo.de>
2022-03-07 17:13:42 +0100

    Add test to PhraseCard

---
+++ src/components/PhraseCard.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export default function PhraseCard() {
+  return (
+   <article>
+     <p>22. Feb. 2022</p>
+     <p>Das ist mein Papapa!</p>
+   </article>
+  );
+}
+
---
+++ src/components/PhraseCard.stories.js
@@ -0,0 +1,8 @@
+import PhraseCard from './PhraseCard.js';
+
+export default {
+  title: 'components/PhraseCard',
+  component: PhraseCard,
+};
+
+export const Default = () => <PhraseCard />;
